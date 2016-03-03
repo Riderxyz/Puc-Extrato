f9dad9309f737781cdafe7bba27daf4c49b46b37
nilton jacintho <nilton.jacintho@gmail.com>
2016-03-03 16:53:09 -0300

    Inclus?o do IONIC

--- ionic/Login/www/js/app.js
+++ ionic/Login/www/js/app.js
@@ -7,7 +7,13 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'ionic-datepicker', 'starter.controllers', 'starter.services'])
 
+.service('UsuarioSrv', [function (codigo) {
+  var usuario = {}
+  usuario.codigo = codigo
+  
+}])
 .run(function($ionicPlatform) {
+
   $ionicPlatform.ready(function($scope) {
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -21,103 +27,114 @@ angular.module('starter', ['ionic', 'ionic-datepicker', 'starter.controllers', '
   });
 })
 
-.controller('LoginCtrl', ['$scope','$http','$location', function($scope, $http, $location) 
- {
-    $scope.data = {};
-    $scope.data.username = '2387';
-    $scope.data.password = '123456';
-    $scope.data.errormessage = 'Informe usuario e senha';
-    $scope.login = function()
-      {
-        $http.get('http://localhost:19017/api/usuarios?_usuario='+$scope.data.username+'&_senha='+$scope.data.password).then(function(data)
-        { 
-          //console.log(JSON.parse(data.nome);
-          $scope.coordenador = JSON.parse(data.data[0]).tab1[0];
-          if ($scope.coordenador.coordenador == $scope.data.username){
-            $scope.errormessage = ' ';
-            $location.path( "/tab" ); 
+.controller('LoginCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+  $scope.data = {};
+  $scope.data.username = '2387';
+  $scope.data.password = '123456';
+  //$rootScope.usuario = 'usuario dentro do controler';
+  $scope.data.errormessage = 'Informe usuario e senha';
+  $scope.login = function() {
+    $http.get('http://localhost:19017/api/usuarios?_usuario=' + $scope.data.username + '&_senha=' + $scope.data.password).then(function(data) {
+      //console.log(JSON.parse(data.nome);
+      $scope.coordenador = JSON.parse(data.data[0]).tab1[0];
+      if ($scope.coordenador.coordenador == $scope.data.username) {
+        $scope.errormessage = ' ';
+        $location.path("/tab");
+      } else {
+        $scope.data.errormessage = 'Usuario ou senha inválidos';
+      }
+    })
+  };
+}])
+
+.controller('ProjetoCtrl', ['$scope', '$http', '$location','$rootScope', function($scope, $http, $location) {
+      var d = new Date();
+      $scope.parmdata = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate()
+      $scope.name = '1'
+//alert(LoginCtrl.data.username)
+      $scope.init = function(name) {
+        $scope.name = name;
+        $http.get("http://localhost:19017/api/projetos?coordenador=" + $scope.name + "&data=" + $scope.parmdata ).then(
+          function(data) {
+            //console.log(JSON.parse(data.nome);
+            $scope.projetos = JSON.parse(data.data[0]);
+            console.log($scope.projetos);
           }
-          else
-          {
-            $scope.data.errormessage = 'Usuario ou senha inválidos';            
-          }
-        })   
-      }; 
+        )
+      }
     }])
 
+    .config(function($stateProvider, $urlRouterProvider) {
 
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-.config(function($stateProvider, $urlRouterProvider) {
+      // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: '/tab',
+        templateUrl: 'templates/tabs.html'
+      })
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+      // Each tab has its own nav history stack:
+      .state('login', {
+          url: '/login',
+          templateUrl: 'templates/login.html',
+          controller: 'LoginCtrl'
+        })
+        .state('tab.dash', {
+          url: '/dash',
+          views: {
+            'tab-dash': {
+              templateUrl: 'templates/tab-dash.html',
+              controller: 'DashCtrl'
+            }
+          }
+        })
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
- .state('login', {
-      url: '/login',
-      templateUrl: 'templates/login.html',
-      controller:'LoginCtrl'
-  })
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.projetos', {
-    url: '/projetos',
-    views: {
-      'tab-projetos': {
-        templateUrl: 'templates/tab-projetos.html',
-        controller: 'ProjetoCtrl'
-      }
-    }
-  })
-
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+      .state('tab.projetos', {
+        url: '/projetos',
+        views: {
+          'tab-projetos': {
+            templateUrl: 'templates/tab-projetos.html',
+            controller: 'ProjetoCtrl'
+          }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+      })
+
+
+      .state('tab.chats', {
+          url: '/chats',
+          views: {
+            'tab-chats': {
+              templateUrl: 'templates/tab-chats.html',
+              controller: 'ChatsCtrl'
+            }
+          }
+        })
+        .state('tab.chat-detail', {
+          url: '/chats/:chatId',
+          views: {
+            'tab-chats': {
+              templateUrl: 'templates/chat-detail.html',
+              controller: 'ChatDetailCtrl'
+            }
+          }
+        })
+
+      .state('tab.account', {
+        url: '/account',
+        views: {
+          'tab-account': {
+            templateUrl: 'templates/tab-account.html',
+            controller: 'AccountCtrl'
+          }
         }
-      }
-    })
+      });
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('login');
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('login'); 
-
-});
+    });
