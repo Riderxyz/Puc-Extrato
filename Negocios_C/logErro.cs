93bd54273c16a6565cd4b7421c7d2cb6d33abc8d
Nilton Cesar <nilton.jacintho@gmail.com>
2016-09-07 22:17:09 -0300

    erro de log

--- Negocios_C/logErro.cs
+++ Negocios_C/logErro.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Negocio;
 
 namespace Puc.Negocios_C
@@ -17,10 +13,10 @@ namespace Puc.Negocios_C
             banco.parametros.Clear();
             banco.parametros.Add(new System.Data.SqlClient.SqlParameter("source", ex.Source));
             banco.parametros.Add(new System.Data.SqlClient.SqlParameter("trace", ex.StackTrace.ToString()));
-            banco.parametros.Add(new System.Data.SqlClient.SqlParameter("modulo", ex.TargetSite.Module.Name));
+            banco.parametros.Add(new System.Data.SqlClient.SqlParameter("modulo", ex.TargetSite.DeclaringType.Module.Name ));
             banco.parametros.Add(new System.Data.SqlClient.SqlParameter("metodo", ex.TargetSite.MethodHandle.Value));
-            banco.parametros.Add(new System.Data.SqlClient.SqlParameter("modulo_versao", ex.TargetSite.ReflectedType.Module.ModuleVersionId));
-            banco.parametros.Add(new System.Data.SqlClient.SqlParameter("fullname", ex.TargetSite.Module.FullyQualifiedName));
+            banco.parametros.Add(new System.Data.SqlClient.SqlParameter("modulo_versao", ex.TargetSite.DeclaringType.Module.ModuleVersionId.ToString()));
+            banco.parametros.Add(new System.Data.SqlClient.SqlParameter("fullname", ex.TargetSite.Module.FullyQualifiedName.ToString()));
             banco.parametros.Add(new System.Data.SqlClient.SqlParameter("namespace", ex.TargetSite.Module.Assembly.FullName));
             banco.ExecuteAndReturnData("sp_LogErrorIncluir", "tablog");
 
